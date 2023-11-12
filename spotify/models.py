675ecf837de4b3d58606bbf60a7c83c364e26c12
Nima HeydariNasab <nima.heydari79@yahoo.com>
2023-11-12 21:54:01 +0330

    End of session 19

--- spotify/models.py
+++ spotify/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class ModelWithTimestamps(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 class Person(models.Model):
@@ -38,3 +48,174 @@ class Post(models.Model):
 
 class JwtIdentifier(models.Model):
     identifier = models.CharField(max_length=255)
+
+
+class Country(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+
+
+class City(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(
+        Country,
+        related_name='cities',
+        on_delete=models.CASCADE
+    )
+
+
+class Like(models.Model):
+    type = models.BooleanField()
+    user = models.ForeignKey(
+        User,
+        related_name='likes',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class Rate(models.Model):
+    type = models.BooleanField()
+    user = models.ForeignKey(
+        User,
+        related_name='rates',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class Artist(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+    image = models.TextField()
+    likes = GenericRelation(Like)
+    rates = GenericRelation(Rate)
+
+
+class Genre(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+
+
+class Album(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+    release_datetime = models.DateTimeField()
+    image = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name='albums')
+    likes = GenericRelation(Like)
+    rates = GenericRelation(Rate)
+
+
+class Cover(ModelWithTimestamps):
+    image = models.TextField()
+    album = models.ForeignKey(
+        Album,
+        related_name='covers',
+        on_delete=models.CASCADE
+    )
+    likes = GenericRelation(Like)
+    rates = GenericRelation(Rate)
+
+
+class Track(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+    duration = models.PositiveIntegerField()
+    lyrics = models.TextField(blank=True, null=True)
+    path_128 = models.TextField()
+    path_320 = models.TextField()
+    album = models.ForeignKey(
+        Album,
+        blank=True,
+        null=True,
+        related_name='tracks',
+        on_delete=models.CASCADE
+    )
+    artist = models.ForeignKey(
+        Artist,
+        related_name='tracks',
+        on_delete=models.CASCADE
+    )
+    likes = GenericRelation(Like)
+    rates = GenericRelation(Rate)
+
+
+class Playlist(ModelWithTimestamps):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        User,
+        related_name='playlists',
+        on_delete=models.CASCADE
+    )
+    tracks = models.ManyToManyField(Track, through='PlaylistTrack')
+    likes = GenericRelation(Like)
+    rates = GenericRelation(Rate)
+
+
+class PlaylistTrack(ModelWithTimestamps):
+    playlist = models.ForeignKey(
+        Playlist,
+        related_name='playlist_tracks',
+        on_delete=models.CASCADE
+    )
+    track = models.ForeignKey(
+        Track,
+        related_name='playlist_tracks',
+        on_delete=models.CASCADE
+    )
+
+
+class Subscription(ModelWithTimestamps):
+    title = models.CharField(max_length=255)
+    duration = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class UserSubscription(ModelWithTimestamps):
+    user = models.ForeignKey(
+        User,
+        related_name='user_subscriptions',
+        on_delete=models.CASCADE
+    )
+    subscription = models.ForeignKey(
+        Subscription,
+        related_name='user_subscriptions',
+        on_delete=models.CASCADE
+    )
+
+
+class Coupon(ModelWithTimestamps):
+    COUPON_TYPES = (
+        ('p', 'percent'),
+        ('a', 'amount'),
+    )
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=1, choices=COUPON_TYPES)
+    value = models.PositiveIntegerField()
+    max_amount = models.PositiveIntegerField(blank=True, null=True)
+    max_usage_count = models.PositiveIntegerField(blank=True, null=True)
+    max_usage_count_per_user = models.PositiveIntegerField(blank=True, null=True)
+    only_for_first_transaction = models.BooleanField(default=False)
+    valid_from = models.DateTimeField(blank=True, null=True)
+    valid_until = models.DateTimeField(blank=True, null=True)
+
+
+class Voucher(ModelWithTimestamps):
+    subscription = models.ForeignKey(
+        Subscription,
+        related_name='vouchers',
+        on_delete=models.CASCADE
+    )
+
+
+class VoucherRedeem(ModelWithTimestamps):
+    user = models.ForeignKey(
+        User,
+        related_name='voucher_redeems',
+        on_delete=models.CASCADE
+    )
+    voucher = models.ForeignKey(
+        Voucher,
+        related_name='voucher_redeems',
+        on_delete=models.CASCADE
+    )
--- spotify/settings.py
+++ spotify/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'spotify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
